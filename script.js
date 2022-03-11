790dde0b146ad61a871ee1385e9717c49b404990
Yana <Yana2894mig@mail.ru>
2022-03-11 08:53:32 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+"use srict";
+
+const answers = [];
+
+answers[0] = prompt("Как вас зовут?", "");
+answers[1] = prompt("Как ваше фамилия?", "");
+answers[2] = prompt("Сколько лет?", "");
+
+document.write(answers);
+
+
