86e2d422ea8e06865046316443c3739187ac2354
Yana <Yana2894mig@mail.ru>
2022-04-20 15:37:30 +0300

    maksimka@DESKTOP-8V3MK93 MINGW64 /e/_Yanchik/JavaScript/Project_1 (master) $ git add Nothing specified, nothing added. Maybe you wanted to say 'git add .'?
    maksimka@DESKTOP-8V3MK93 MINGW64 /e/_Yanchik/JavaScript/Project_1 (master)
    $ git A
    git: 'A' is not a git command. See 'git --help'.
    
    The most similar commands are
            am
            gc
            mv
            p4
            rm
    
    maksimka@DESKTOP-8V3MK93 MINGW64 /e/_Yanchik/JavaScript/Project_1 (master)
    $ git add -A
    
    maksimka@DESKTOP-8V3MK93 MINGW64 /e/_Yanchik/JavaScript/Project_1 (master)
    $ git commit -a -m

--- script.js
+++ script.js
@@ -1,75 +1,75 @@
 "use strict";
 
-let numberOfFilms ;
-
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов просмотрели?","");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)){
-        numberOfFilms = +prompt("Сколько фильмов просмотрели?","");
-    }
-}
-
-start();
 
 const personalMovieDB = {
-    count: numberOfFilms,
+    count: 0,
     movies: {},
     actors: {},
     genres: [],
     privat: false,
-};
-
- function rememberMyFilms() {
-    for(let i=0; i < 2; i++){
-        const a = prompt('Один из последних просмотренных фильмов?', ""),
-              b = prompt("Насколько оцените его?","");
+    start: function() {
+        personalMovieDB.count = +prompt("Сколько фильмов просмотрели?","");
     
-        if (a != null && b != null && a != " " && b != " " && a.length <50){
-            personalMovieDB.movies[a] = b;
-            console.log("done");
-        } else {
-            console.log("error");
-            i--;
+        while (personalMovieDB.count== "" || personalMovieDB.count == null || isNaN(personalMovieDB.count)){
+            personalMovieDB.count = +prompt("Сколько фильмов просмотрели?","");
         }
-    }
- }
-
- rememberMyFilms();
-
- function detectPersonalLevel() {
-    if (personalMovieDB.count < 10){
-        console.log("Просмотрено довольно мало фильмов");
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-        console.log("Вы классический зритель");
-    } else  if (personalMovieDB.count>= 30){
-        console.log("Вы киноман");
-    } else{
-        console.log("Произошла ошибка");
-    }
- }
- detectPersonalLevel();
-
-console.log(personalMovieDB);
-
- function showMyDB(){
-     if (!personalMovieDB.privat) {
-        console.log(personalMovieDB);
-     }
- }
- showMyDB();
-
- function writeYourGenres (){
+    },
+    rememberMyFilms: function() {
+        for(let i=0; i < 2; i++){
+            const a = prompt('Один из последних просмотренных фильмов?', ""),
+                  b = prompt("Насколько оцените его?","");
+        
+            if (a != null && b != null && a != " " && b != " " && a.length <50){
+                personalMovieDB.movies[a] = b;
+                console.log("done");
+            } else {
+                console.log("error");
+                i--;
+            }
+        }
+     },
+    detectPersonalLevel: function() {
+        if (personalMovieDB.count < 10){
+            console.log("Просмотрено довольно мало фильмов");
+        } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+            console.log("Вы классический зритель");
+        } else  if (personalMovieDB.count>= 30){
+            console.log("Вы киноман");
+        } else{
+            console.log("Произошла ошибка");
+        }
+     },
+    showMyDB: function() {
+        if (!personalMovieDB.privat) {
+           console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function(){
       
-      for(let i = 1; i < 4; i++){
-    //    const  favoriteGenre = prompt("Ваш любимый жанр под номером "+ i, "");
-    //     personalMovieDB.genres.push(favoriteGenre);
+        for(let i = 1; i < 4; i++){
+      //    const  favoriteGenre = prompt("Ваш любимый жанр под номером "+ i, "");
+      //     personalMovieDB.genres.push(favoriteGenre);
+        let genre = prompt("Ваш любимый жанр под номером "+ i, "");
+        if(genre == "" ||  genre === null) {
+            console.log ("вы ввели некорректные данные или не ввели их вообще");
+            i--;
+        } else {
+          personalMovieDB.genres[i - 1] = genre;
+        }
 
-        personalMovieDB.genres[i - 1] = prompt("Ваш любимый жанр под номером "+ i, "");
-      }
- }
+        personalMovieDB.genres.forEach((item, i) =>{
+            console.log(`Любимый жанр ${i + 1} - это ${item}`)
+        });
+    }
+   },
 
- writeYourGenres();
- 
-
-
+   toggleVisibleMyDB: function() {
+    // personalMovieDB.privat = !personalMovieDB.privat;
+       if (personalMovieDB.privat) {
+        personalMovieDB.privat = false;
+       } else {
+        personalMovieDB.privat = true;
+       }
+    // personalMovieDB.privat = (personalMovieDB.privat == false ? true : false); 
+   },
+};
